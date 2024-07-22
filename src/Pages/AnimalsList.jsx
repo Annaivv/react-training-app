ea5738c912e15434fb80e0c90be69b1699e6430b
Annaivv <annashalya@gmail.com>
2024-07-22 17:40:42 +0300

    feature: removing animal from list added

--- src/Pages/AnimalsList.jsx
+++ src/Pages/AnimalsList.jsx
@@ -5,16 +5,21 @@ import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import IconButton from "@mui/material/IconButton";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import ClearIcon from "@mui/icons-material/Clear";
 import { getAnimals } from "../fakeAPI";
 import { Link, useLocation } from "react-router-dom";
 import { AddAnimalForm } from "../Components/AddAnimalForm";
+import { Typography } from "@mui/material";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(2),
   textAlign: "center",
+  display: "flex",
+  justifyContent: "space-between",
   color: theme.palette.text.secondary,
+  //maxWidth: 345,
 }));
 
 export const Animals = () => {
@@ -37,19 +42,35 @@ export const Animals = () => {
     });
   };
 
+  const handleRemoveAnimal = (id) => {
+    setAnimals((prevAnimals) => {
+      const updatedAnimals = prevAnimals.filter((animal) => animal.id !== id);
+      localStorage.setItem("animals", JSON.stringify(updatedAnimals));
+      return updatedAnimals;
+    });
+  };
+
   return (
     <>
       <Box sx={{ width: "100%", paddingTop: "24px", textAlign: "center" }}>
         <Stack spacing={2}>
           {animals.map((animal) => (
-            <Link
-              to={`/animals/${animal.id}`}
-              state={{ from: location }}
-              key={animal.id}
-              sx={{ textDecoration: "none" }}
-            >
-              <Item>{animal.name}</Item>
-            </Link>
+            <Item key={animal.id}>
+              <Link
+                to={`/animals/${animal.id}`}
+                state={{ from: location }}
+                style={{ textDecoration: "none", flexGrow: 1 }}
+              >
+                <Typography color="primary">{animal.name}</Typography>
+              </Link>
+              <IconButton
+                color="error"
+                aria-label="remove animal"
+                onClick={() => handleRemoveAnimal(animal.id)}
+              >
+                <ClearIcon fontSize="medium" />
+              </IconButton>
+            </Item>
           ))}
         </Stack>
         <IconButton
