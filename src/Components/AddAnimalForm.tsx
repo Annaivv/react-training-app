3553d521570d40765fd53fcec71a4488ef93ab41
Annaivv <annashalya@gmail.com>
2024-07-28 19:34:55 +0300

    feature: MaterialUI components added to react-hook-form (without picture upload)

--- src/Components/AddAnimalForm.tsx
+++ src/Components/AddAnimalForm.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { nanoid } from "nanoid";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
@@ -27,6 +27,7 @@ export const AddNewAnimalForm: React.FC<AddNewAnimalFormProps> = ({
   handleAddAnimal,
 }) => {
   const {
+    control,
     register,
     handleSubmit,
     watch,
@@ -54,27 +55,84 @@ export const AddNewAnimalForm: React.FC<AddNewAnimalFormProps> = ({
           Please fill in the information about your animal.
         </DialogContentText>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <TextField
-            {...register("name", { required: "This is required field" })}
-            autoFocus
-            required
-            margin="dense"
-            id="name"
+          <Controller
+            {...register("name", {
+              required: "This field is required",
+            })}
             name="name"
-            label="Animal Name"
-            type="text"
-            fullWidth
-            variant="standard"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Animal Name"
+                type="text"
+                fullWidth
+                variant="standard"
+                error={!!errors.name}
+                helperText={errors.name ? errors.name.message : ""}
+              />
+            )}
           />
-          {/* <label>Animal Name</label>
-          <input
-            {...register("animalName", { required: "This is required field" })}
-          /> */}
 
-          <label>Animal Age</label>
-          <input type="number" {...register("age")} />
-          <label>Animal Description</label>
-          <input {...register("description")} />
+          <Controller
+            {...register("age", {
+              required: "This field is required",
+              valueAsNumber: true,
+              validate: (v) =>
+                (v !== undefined && v > 0) || "Age must be a positive number",
+            })}
+            name="age"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                margin="dense"
+                id="age"
+                label="Animal Age (in years)"
+                type="number"
+                fullWidth
+                variant="standard"
+                error={!!errors.age}
+                helperText={errors.age ? errors.age.message : ""}
+                inputProps={{ min: 1 }}
+              />
+            )}
+          />
+
+          <Controller
+            {...register("description")}
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                margin="dense"
+                id="description"
+                label="Animal Description"
+                type="text"
+                fullWidth
+                multiline
+                variant="standard"
+              />
+            )}
+          />
+
+          {/* <input
+            accept="image/*"
+            style={{ display: "none" }}
+            id="photo"
+            name="animalPhoto"
+            type="file"
+          />
+          <label htmlFor="photo">
+            <Button variant="contained" component="span">
+              Upload Photo
+            </Button>
+          </label> */}
+
           <DialogActions>
             <Button onClick={handleCloseForm}>Cancel</Button>
             <Button type="submit">Add Animal</Button>
@@ -84,77 +142,3 @@ export const AddNewAnimalForm: React.FC<AddNewAnimalFormProps> = ({
     </Dialog>
   );
 };
-
-// export const AddAnimalForm = ({ open, handleCloseForm, handleAddAnimal }) => {
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-
-//     const formData = new FormData(event.currentTarget);
-//     const formJson = Object.fromEntries(formData.entries());
-//     const name = formJson.animalName;
-//     const age = formJson.animalAge;
-//     const description = formJson.animalDescription;
-//     const newAnimal = { id: nanoid(), name, age, description };
-//     handleAddAnimal(newAnimal);
-//     handleCloseForm();
-//   };
-
-//   return (
-//     <Dialog open={open} onClose={handleCloseForm}>
-//       <form onSubmit={handleSubmit}>
-//         <DialogTitle>Add Your Animal</DialogTitle>
-//         <DialogContent>
-//           <DialogContentText>
-//             Please fill in the information about your animal.
-//           </DialogContentText>
-//           <TextField
-//             autoFocus
-//             required
-//             margin="dense"
-//             id="name"
-//             name="animalName"
-//             label="Animal Name"
-//             type="text"
-//             fullWidth
-//             variant="standard"
-//           />
-//           <TextField
-//             margin="dense"
-//             id="age"
-//             name="animalAge"
-//             label="Animal Age (in years)"
-//             type="number"
-//             fullWidth
-//             variant="standard"
-//           />
-//           <TextField
-//             margin="dense"
-//             id="description"
-//             name="animalDescription"
-//             label="Animal Description"
-//             type="text"
-//             fullWidth
-//             multiline
-//             variant="standard"
-//           />
-//           <input
-//             accept="image/*"
-//             style={{ display: "none" }}
-//             id="photo"
-//             name="animalPhoto"
-//             type="file"
-//           />
-//           <label htmlFor="photo">
-//             <Button variant="contained" component="span">
-//               Upload Photo
-//             </Button>
-//           </label>
-//         </DialogContent>
-//         <DialogActions>
-//           <Button onClick={handleCloseForm}>Cancel</Button>
-//           <Button type="submit">Add Animal</Button>
-//         </DialogActions>
-//       </form>
-//     </Dialog>
-//   );
-// };
