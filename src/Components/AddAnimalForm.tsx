a689337020bfc80eb4cd39916420d79551d088ba
Annaivv <annashalya@gmail.com>
2024-08-19 00:14:26 +0300

    feature: common form component added, AddNewAnimalForm updated

--- src/Components/AddAnimalForm.tsx
+++ src/Components/AddAnimalForm.tsx
@@ -1,25 +1,8 @@
 import * as React from "react";
-import { nanoid } from "nanoid";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
-import Button from "@mui/material/Button";
+import { useForm, Controller } from "react-hook-form";
 import TextField from "@mui/material/TextField";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-
-interface IFormInput {
-  name: string;
-  age: number;
-  description?: string;
-}
-
-interface AddNewAnimalFormProps {
-  open: boolean;
-  handleCloseForm: () => void;
-  handleAddAnimal: (newAnimal: IFormInput & { id: string }) => void;
-}
+import { AddNewItemForm } from "./AddItemForm";
+import { AnimalFormInput, AddNewAnimalFormProps } from "../commonTypes";
 
 export const AddNewAnimalForm: React.FC<AddNewAnimalFormProps> = ({
   open,
@@ -28,100 +11,157 @@ export const AddNewAnimalForm: React.FC<AddNewAnimalFormProps> = ({
 }) => {
   const {
     control,
-    handleSubmit,
     formState: { errors },
-  } = useForm<IFormInput>();
-
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    const newAnimal = {
-      id: nanoid(),
-      ...data,
-    };
-    handleAddAnimal(newAnimal);
-    handleCloseForm();
-  };
+  } = useForm<AnimalFormInput>();
 
   return (
-    <Dialog open={open} onClose={handleCloseForm}>
-      <DialogTitle>Add Your Animal</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Please fill in the information about your animal.
-        </DialogContentText>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Controller
-            name="name"
-            control={control}
-            defaultValue=""
-            rules={{
-              required: "This field is required",
-            }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                autoFocus
-                margin="dense"
-                id="name"
-                label="Animal Name"
-                type="text"
-                fullWidth
-                variant="standard"
-                error={!!errors.name}
-                helperText={errors.name ? errors.name.message : ""}
-                value={field.value ?? ""}
-              />
-            )}
+    <AddNewItemForm
+      open={open}
+      handleCloseForm={handleCloseForm}
+      handleAddItem={handleAddAnimal}
+    >
+      <Controller
+        name="age"
+        control={control}
+        defaultValue={undefined}
+        rules={{
+          required: "This field is required",
+          validate: (v) =>
+            (v !== undefined && v > 0) || "Age must be a positive number",
+        }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            margin="dense"
+            id="age"
+            label="Animal Age (in years)"
+            type="number"
+            fullWidth
+            variant="standard"
+            error={!!errors.age}
+            helperText={errors.age ? errors.age.message : ""}
+            inputProps={{ min: 1 }}
+            value={field.value ?? ""}
           />
-
-          <Controller
-            name="age"
-            control={control}
-            defaultValue={undefined}
-            rules={{
-              required: "This field is required",
-              validate: (v) =>
-                (v !== undefined && v > 0) || "Age must be a positive number",
-            }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                margin="dense"
-                id="age"
-                label="Animal Age (in years)"
-                type="number"
-                fullWidth
-                variant="standard"
-                error={!!errors.age}
-                helperText={errors.age ? errors.age.message : ""}
-                inputProps={{ min: 1 }}
-                value={field.value ?? ""}
-              />
-            )}
-          />
-
-          <Controller
-            name="description"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                margin="dense"
-                id="description"
-                label="Animal Description"
-                type="text"
-                fullWidth
-                multiline
-                variant="standard"
-                value={field.value ?? ""}
-              />
-            )}
-          />
-          <DialogActions>
-            <Button onClick={handleCloseForm}>Cancel</Button>
-            <Button type="submit">Add Animal</Button>
-          </DialogActions>
-        </form>
-      </DialogContent>
-    </Dialog>
+        )}
+      />
+    </AddNewItemForm>
   );
 };
+
+// interface IFormInput {
+//   name: string;
+//   age: number;
+//   description?: string;
+// }
+
+// interface AddNewAnimalFormProps {
+//   open: boolean;
+//   handleCloseForm: () => void;
+//   handleAddAnimal: (newAnimal: IFormInput & { id: string }) => void;
+// }
+
+// export const AddNewAnimalForm: React.FC<AddNewAnimalFormProps> = ({
+//   open,
+//   handleCloseForm,
+//   handleAddAnimal,
+// }) => {
+//   const {
+//     control,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<IFormInput>();
+
+//   const onSubmit: SubmitHandler<IFormInput> = (data) => {
+//     const newAnimal = {
+//       id: nanoid(),
+//       ...data,
+//     };
+//     handleAddAnimal(newAnimal);
+//     handleCloseForm();
+//   };
+
+//   return (
+//     <Dialog open={open} onClose={handleCloseForm}>
+//       <DialogTitle>Add Your Animal</DialogTitle>
+//       <DialogContent>
+//         <DialogContentText>
+//           Please fill in the information about your animal.
+//         </DialogContentText>
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//           <Controller
+//             name="name"
+//             control={control}
+//             defaultValue=""
+//             rules={{
+//               required: "This field is required",
+//             }}
+//             render={({ field }) => (
+//               <TextField
+//                 {...field}
+//                 autoFocus
+//                 margin="dense"
+//                 id="name"
+//                 label="Animal Name"
+//                 type="text"
+//                 fullWidth
+//                 variant="standard"
+//                 error={!!errors.name}
+//                 helperText={errors.name ? errors.name.message : ""}
+//                 value={field.value ?? ""}
+//               />
+//             )}
+//           />
+
+//           <Controller
+//             name="age"
+//             control={control}
+//             defaultValue={undefined}
+//             rules={{
+//               required: "This field is required",
+//               validate: (v) =>
+//                 (v !== undefined && v > 0) || "Age must be a positive number",
+//             }}
+//             render={({ field }) => (
+//               <TextField
+//                 {...field}
+//                 margin="dense"
+//                 id="age"
+//                 label="Animal Age (in years)"
+//                 type="number"
+//                 fullWidth
+//                 variant="standard"
+//                 error={!!errors.age}
+//                 helperText={errors.age ? errors.age.message : ""}
+//                 inputProps={{ min: 1 }}
+//                 value={field.value ?? ""}
+//               />
+//             )}
+//           />
+
+//           <Controller
+//             name="description"
+//             control={control}
+//             render={({ field }) => (
+//               <TextField
+//                 {...field}
+//                 margin="dense"
+//                 id="description"
+//                 label="Animal Description"
+//                 type="text"
+//                 fullWidth
+//                 multiline
+//                 variant="standard"
+//                 value={field.value ?? ""}
+//               />
+//             )}
+//           />
+//           <DialogActions>
+//             <Button onClick={handleCloseForm}>Cancel</Button>
+//             <Button type="submit">Add Animal</Button>
+//           </DialogActions>
+//         </form>
+//       </DialogContent>
+//     </Dialog>
+//   );
+// };
--- src/commonTypes.tsx
+++ src/commonTypes.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface Item {
   id: string;
   name: string;
@@ -10,4 +12,25 @@ export interface Animal extends Item {
 
 export interface Exercise extends Item {
   description?: string;
+}
+
+export interface FormInput {
+  name: string;
+  description?: string;
+}
+
+export interface AnimalFormInput extends FormInput {
+  age: number;
+}
+
+export interface AddNewItemFormProps {
+  open: boolean;
+  handleCloseForm: () => void;
+  handleAddItem: (item: any) => void;
+  children?: React.ReactNode;
+}
+
+export interface AddNewAnimalFormProps
+  extends Omit<AddNewItemFormProps, "handleAddItem" | "children"> {
+  handleAddAnimal: (newAnimal: Animal) => void;
 }
